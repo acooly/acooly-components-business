b92ced33707726bfd5e3a0fedc845f56c4727c4e
shuijing <shuijing@yiji.com>
2018-09-20 17:30:11 +0800

    修复字段创建bug

--- acooly-component-tests/src/main/java/com/acooly/module/test/Main.java
+++ acooly-component-tests/src/main/java/com/acooly/module/test/Main.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 /**
  * @author qiubo
  */
-@BootApp(sysName = "acooly-components-test", httpPort = 8080)
+@BootApp(sysName = "acooly-components-test", httpPort = 8040)
 @ComponentScan({"com.acooly.module.member"})
 public class Main {
     public static void main(String[] args) {
