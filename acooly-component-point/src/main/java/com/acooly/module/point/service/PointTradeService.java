a468d15749c4518e1dc716773275cb2614bc8dca
cuifuq7 <cuifuq7@gmail.com>
2022-12-09 14:14:23 +0800

    新增积分交易模块，支持积分转让

--- acooly-component-point/src/main/java/com/acooly/module/point/service/PointTradeService.java
+++ acooly-component-point/src/main/java/com/acooly/module/point/service/PointTradeService.java
@@ -21,101 +21,115 @@ import com.acooly.module.point.entity.PointTrade;
  */
 public interface PointTradeService extends EntityService<PointTrade> {
 
-	/**
-	 * 积分产生
-	 *
-	 * @param userNo        用户号
-	 * @param userName      用户名
-	 * @param point         交易积分(产生)
-	 * @param overdueDate   过期时间，格式：yyyy-MM-dd; 为空 :无限期； 第二天清零前一天的积分
-	 * @param pointTradeDto 业务数据
-	 */
-	public PointTrade pointProduce(String userNo, String userName, long point, String overdueDate,
-			PointTradeInfoDto pointTradeDto);
+    /**
+     * 积分产生
+     *
+     * @param userNo        用户号
+     * @param userName      用户名
+     * @param point         交易积分(产生)
+     * @param overdueDate   过期时间，格式：yyyy-MM-dd; 为空 :无限期； 第二天清零前一天的积分
+     * @param pointTradeDto 业务数据
+     */
+    public PointTrade pointProduce(String userNo, String userName, long point, String overdueDate,
+                                   PointTradeInfoDto pointTradeDto);
 
-	/**
-	 * 积分消费
-	 *
-	 * @param userNo        用户号
-	 * @param userName      用户名
-	 * @param point         交易积分(消费)
-	 * @param isFreeze      是否存在冻结（true:存在冻结积分,false:不存在冻结积分）
-	 * @param pointTradeDto 业务数据
-	 * @return
-	 */
-	public PointTrade pointExpense(String userNo, String userName, long point, boolean isFreeze,
-			PointTradeInfoDto pointTradeDto);
+    /**
+     * 积分消费
+     *
+     * @param userNo        用户号
+     * @param userName      用户名
+     * @param point         交易积分(消费)
+     * @param isFreeze      是否存在冻结（true:存在冻结积分,false:不存在冻结积分）
+     * @param pointTradeDto 业务数据
+     * @return
+     */
+    public PointTrade pointExpense(String userNo, String userName, long point, boolean isFreeze,
+                                   PointTradeInfoDto pointTradeDto);
 
-	/**
-	 * 
-	 * @param userNo
-	 * @param userName
-	 * @param totalClearPoint    本次清零积分
-	 * @return
-	 */
-	public PointTrade pointClear(String userNo, String userName, long totalClearPoint, PointTradeInfoDto pointTradeDto);
+    /**
+     * @param userNo
+     * @param userName
+     * @param totalClearPoint 本次清零积分
+     * @return
+     */
+    public PointTrade pointClear(String userNo, String userName, long totalClearPoint, PointTradeInfoDto pointTradeDto);
 
-	/**
-	 * 积分消费--取消
-	 *
-	 * @param userNo        用户号
-	 * @param userName      用户名
-	 * @param point         交易积分(消费)
-	 * @param pointTradeDto 业务数据
-	 * @return
-	 */
-	public PointTrade pointExpenseByCancel(String userNo, String userName, long point, PointTradeInfoDto pointTradeDto);
+    /**
+     * 积分消费--取消
+     *
+     * @param userNo        用户号
+     * @param userName      用户名
+     * @param point         交易积分(消费)
+     * @param pointTradeDto 业务数据
+     * @return
+     */
+    public PointTrade pointExpenseByCancel(String userNo, String userName, long point, PointTradeInfoDto pointTradeDto);
 
-	/**
-	 * 积分冻结
-	 *
-	 * @param userNo        用户号
-	 * @param userName      用户名
-	 * @param point         交易积分(冻结积分)
-	 * @param pointTradeDto 业务数据
-	 * @return
-	 */
-	public PointTrade pointFreeze(String userNo, String userName, long point, PointTradeInfoDto pointTradeDto);
+    /**
+     * 积分冻结
+     *
+     * @param userNo        用户号
+     * @param userName      用户名
+     * @param point         交易积分(冻结积分)
+     * @param pointTradeDto 业务数据
+     * @return
+     */
+    public PointTrade pointFreeze(String userNo, String userName, long point, PointTradeInfoDto pointTradeDto);
 
-	/**
-	 * 积分解冻
-	 *
-	 * @param userNo        用户号
-	 * @param userName      用户名
-	 * @param point         交易积分(解冻积分)
-	 * @param pointTradeDto 业务数据
-	 * @return
-	 */
-	public PointTrade pointUnfreeze(String userNo, String userName, long point, PointTradeInfoDto pointTradeDto);
+    /**
+     * 积分解冻
+     *
+     * @param userNo        用户号
+     * @param userName      用户名
+     * @param point         交易积分(解冻积分)
+     * @param pointTradeDto 业务数据
+     * @return
+     */
+    public PointTrade pointUnfreeze(String userNo, String userName, long point, PointTradeInfoDto pointTradeDto);
 
-	/**
-	 * 获取某一时间产生积分
-	 *
-	 * @param userNo
-	 * @param startTime
-	 * @param endTime
-	 * @return
-	 */
-	public long getProducePoint(String userNo, String startTime, String endTime);
+    /**
+     * 获取某一时间产生积分
+     *
+     * @param userNo
+     * @param startTime
+     * @param endTime
+     * @return
+     */
+    public long getProducePoint(String userNo, String startTime, String endTime);
 
-	/**
-	 * 获取某一时间产生消耗
-	 *
-	 * @param userNo
-	 * @param startTime
-	 * @param endTime
-	 * @return
-	 */
-	public long getExpensePoint(String userNo, String startTime, String endTime);
+    /**
+     * 获取某一时间产生消耗
+     *
+     * @param userNo
+     * @param startTime
+     * @param endTime
+     * @return
+     */
+    public long getExpensePoint(String userNo, String startTime, String endTime);
 
-	/**
-	 * 根据业务类型统计用户累计积分
-	 *
-	 * @param userNo
-	 * @param startTime
-	 * @param endTime
-	 * @return
-	 */
-	public long countProducePointByBusi(String userNo, String busiId, String busiType);
+    /**
+     * 根据业务类型统计用户累计积分
+     *
+     * @param userNo
+     * @param startTime
+     * @param endTime
+     * @return
+     */
+    public long countProducePointByBusi(String userNo, String busiId, String busiType);
 
+    /**
+     * 积分交易（业务系统自行计算积分）
+     *
+     * @param oldUserNo
+     * @param oldUserName
+     * @param tradePoint     交易积分
+     * @param isFreeze       是否存在冻结（true:存在冻结积分,false:不存在冻结积分）
+     *                       true：解冻后执行积分交易
+     *                       false：直接执行积分交易
+     * @param targetUserNo   目标用户号
+     * @param targetUserName 目标用户名
+     * @param pointTradeDto  业务数据
+     * @return
+     */
+    void pointTrade(String oldUserNo, String oldUserName, long tradePoint, boolean isFreeze, String targetUserNo, String targetUserName, PointTradeInfoDto pointTradeDto);
 }
